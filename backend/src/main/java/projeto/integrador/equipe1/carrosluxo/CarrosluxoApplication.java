0353547b0fc0dee8385996070916d4702575f606
ViniciusOcker <60163731+ViniciusOcker@users.noreply.github.com>
2023-02-15 19:12:10 -0300

    iniciando back...

---
+++ backend/src/main/java/projeto/integrador/equipe1/carrosluxo/CarrosluxoApplication.java
@@ -0,0 +1,13 @@
+package projeto.integrador.equipe1.carrosluxo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarrosluxoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarrosluxoApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/projeto/integrador/equipe1/carrosluxo/CarrosluxoApplicationTests.java
@@ -0,0 +1,13 @@
+package projeto.integrador.equipe1.carrosluxo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarrosluxoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
