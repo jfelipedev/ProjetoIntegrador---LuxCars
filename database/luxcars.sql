3d47f2967880faf1cc04799f4912e435afb92481
camila <cftinelli@outlook.com>
2023-02-18 14:14:23 -0300

    alteracao da coluna pass e entidade categories

--- database/luxcars.sql
+++ database/luxcars.sql
@@ -6,7 +6,7 @@ CREATE TABLE customers (
 
 id_customer BIGINT (20) AUTO_INCREMENT NOT NULL PRIMARY KEY,
 email VARCHAR (200) NOT NULL,
-password VARCHAR (50) NOT NULL, 
+password VARCHAR (60) NOT NULL, 
 first_name VARCHAR (100) NOT NULL,
 surname VARCHAR (100) NOT NULL
 
@@ -15,7 +15,9 @@ surname VARCHAR (100) NOT NULL
 CREATE TABLE categories (
 
 id_category BIGINT (20) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-descritpion VARCHAR (200) NOT NULL
+descritpion VARCHAR (200) NOT NULL,
+url_image VARCHAR (255) NOT NULL,
+model VARCHAR (100) NOT NULL
 
 );
 
@@ -23,7 +25,6 @@ CREATE TABLE car (
 
 id_car BIGINT (20) NOT NULL AUTO_INCREMENT PRIMARY KEY,
 name_car VARCHAR (100) NOT NULL,
-description VARCHAR (100) NOT NULL,
 category_id BIGINT,
 FOREIGN KEY (category_id) REFERENCES categories(id_category)
 
