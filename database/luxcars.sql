638a5890b4e01cd25e0ad1ac6634cca1ae029814
camila <cftinelli@outlook.com>
2023-02-18 13:56:14 -0300

    alteracao do sql do BD apenas com estrutura da sprint 1

---
+++ database/luxcars.sql
@@ -0,0 +1,33 @@
+CREATE DATABASE luxcars;
+
+USE luxcars;
+
+CREATE TABLE customers (
+
+id_customer BIGINT (20) AUTO_INCREMENT NOT NULL PRIMARY KEY,
+email VARCHAR (50) NOT NULL,
+password VARCHAR (50) NOT NULL, 
+first_name VARCHAR (50) NOT NULL,
+surname VARCHAR (50) NOT NULL
+
+);
+
+CREATE TABLE categories (
+
+id_category BIGINT (20) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+descritpion VARCHAR (200) NOT NULL
+
+);
+
+CREATE TABLE car (
+
+id_car BIGINT (20) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+name_car VARCHAR (100) NOT NULL,
+model VARCHAR (100) NOT NULL,
+year_car DATE NOT NULL,
+url_image VARCHAR (255) NOT NULL,
+price DOUBLE (14,5) NOT NULL,
+category_id BIGINT,
+FOREIGN KEY (category_id) REFERENCES categories(id_category)
+
+);
