6b834c99ea42ea54a2d34319ff5ec699ac6d5d5f
LAYASUARES <ladislaualexandre940@gmail.com>
2023-03-20 00:43:13 -0300

    Menu hamburguer

--- frontend/src/components/headerModal/headerModal.jsx
+++ frontend/src/components/headerModal/headerModal.jsx
@@ -4,10 +4,10 @@ import { Link } from "react-router-dom";
 import { useEffect, useRef, useState } from "react";
 import { getToken } from "../../services/auth";
 
-const HeaderModal= ({onClose = () => {}}) => {
+const HeaderModal= ({id="headerModal", onClose = () => {}}) => {
+
   const navRef = useRef();
  
-
   const [token, setToken] = useState(getToken());
 
   useEffect(() => {
@@ -19,20 +19,24 @@ const HeaderModal= ({onClose = () => {}}) => {
     return () => window.removeEventListener("storage", handleStorage());
   }, []);
 
+  const handleOutsideClick = (e) => {
+     if(e.target.id === id) onClose();
+  }
+
   return (
-    <div className="headerModal" onClick={onClose}>
+    <div id={id} className="headerModal" onClick={handleOutsideClick}>
       <div className="headerModalcontent">
         <div className="headerModalHeader">
           <i class="uil uil-times navClose" onClick={onClose}></i>
           <div className="headerModalUser">
-            {token && <h1>Ola</h1>}
-            <h1 className="headerModalTitle">Menu</h1>
+            {token && <h1 className="headerModalTitleUser">Ola</h1>}
+            {!token && <h1 className="headerModalTitle">Menu</h1>}
           </div>
         </div>
         <div className="headerModalBody">
-          <ul className="navListModal" ref={navRef}>
-            <li className="navItemModal">
-              <Link to="/" className="navLinkModal activeLink">
+          <ul className="navListModal " ref={navRef}>
+            <li className="navItemModal first">
+              <Link to="/" className="navLinkModal activeLink1">
                 Carros
               </Link>
             </li>
@@ -55,11 +59,14 @@ const HeaderModal= ({onClose = () => {}}) => {
               </Link>
             </li>
 
+            {token && (
             <li className="navItemModal">
                <Link to="/" className="navLink1Modal">
                     Minhas Reservas
                </Link>
             </li>
+            )}
+            
 
             {!token && (
             <li className="navItem1Modal">
@@ -68,11 +75,20 @@ const HeaderModal= ({onClose = () => {}}) => {
                </Link>
             </li>
             )}
+
+            {!token && (
+            <li className="navItem1Modal">
+               <Link to="/createAccount" className="navLink1Modal">
+                    Criar Conta
+               </Link>
+            </li>
+            )}
+           
             
           </ul>
         </div>
         <div className="headerModalFooter">
-          <button className="headerButton"  >Sair da Conta</button>
+          {token && <button className="headerButton button" >Sair da Conta</button>}
         </div>
       </div>
     </div>
