6c2595cc8710f528e9d83d23a5ee6aa3d9e48728
camila <cftinelli@outlook.com>
2023-03-20 21:58:58 -0300

    correcao das tabelas carros e reservas

--- database/luxcars.sql
+++ database/luxcars.sql
@@ -4,7 +4,7 @@ USE luxcars;
 
 CREATE TABLE IF NOT EXISTS roles (
 ID BIGINT (20) AUTO_INCREMENT NOT NULL PRIMARY KEY,
-name_role VARCHAR(100) NOT NULL
+name_role VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS users (
@@ -36,13 +36,6 @@ country VARCHAR(100) NOT NULL
 
 );
 
-CREATE TABLE IF NOT EXISTS bookings (
-ID BIGINT (20) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-start_date DATE,
-start_time TIME,
-end_date DATE
-
-);
 
 
 CREATE TABLE IF NOT EXISTS cars (
@@ -54,12 +47,21 @@ year_car INT (4) NOT NULL,
 highlight BOOLEAN DEFAULT false NOT NULL,
 category_id BIGINT NOT NULL,
 city_id BIGINT NOT NULL,
-user_id BIGINT NOT NULL,
-booking_id BIGINT NOT NULL,
 FOREIGN KEY (category_id) REFERENCES categories(ID),
-FOREIGN KEY(city_id) REFERENCES cities(ID),
-FOREIGN KEY(user_id) REFERENCES users(ID),
-FOREIGN KEY(booking_id) REFERENCES bookings(ID)
+FOREIGN KEY(city_id) REFERENCES cities(ID)
+
+);
+
+CREATE TABLE IF NOT EXISTS bookings (
+ID BIGINT (20) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+start_date DATE,
+start_time TIME,
+end_date DATE,
+car_id BIGINT NOT NULL,
+user_id BIGINT NOT NULL,
+FOREIGN KEY(car_id) REFERENCES cars(ID),
+FOREIGN KEY(user_id) REFERENCES users(ID)
+
 );
 
 
@@ -96,29 +98,29 @@ FOREIGN KEY (car_id) REFERENCES cars(ID)
   Senha do role user = Beltrano12!
 */
 
-insert into roles(name_role) values ('admin');
-insert into roles(name_role) values ('user');
+INSERT INTO roles (name_role) VALUES ('ROLE_ADMIN');
+INSERT INTO roles (name_role) VALUES ('ROLE_USER');
 
-INSERT INTO users(email, password, first_name, surname, roles_id) values ('administrador@mail.com', '$2a$12$IOK8StaznX4/43zA3vPtHOV40wW5APGLpvZh31GiexOYH5B57jTZO', 'Camila', 'Ferreira', (SELECT ID FROM roles WHERE name_role = 'admin'));
-INSERT INTO users(email, password, first_name, surname, roles_id) values ('usercomum@mail.com', '$2a$12$CAlvUt4F4IR4d/rv.hMPWeZC16cGGmGxGB4m37GogRBysdpqj3DfO', 'Beltrano', 'de tal', (SELECT ID FROM roles WHERE name_role = 'user'));
+INSERT INTO users(email, password, first_name, surname, roles_id) values ('administrador@mail.com', '$2a$12$IOK8StaznX4/43zA3vPtHOV40wW5APGLpvZh31GiexOYH5B57jTZO', 'Camila', 'Ferreira', (SELECT ID FROM roles WHERE name_role = 'ROLE_ADMIN'));
+INSERT INTO users(email, password, first_name, surname, roles_id) values ('usercomum@mail.com', '$2a$12$CAlvUt4F4IR4d/rv.hMPWeZC16cGGmGxGB4m37GogRBysdpqj3DfO', 'Beltrano', 'de tal', (SELECT ID FROM roles WHERE name_role = 'ROLE_USER'));
 
 /*insercao nas tabelas de catergories*/
-INSERT INTO categories(descritpion, url_image, qualification) values ('ferrari gt', 'urlimagemaqui', 'conversivel');
+INSERT INTO categories(descritpion, url_image, qualification) values ('ferrari gt', '', 'conversivel');
 
 /*insercao nas tabelas de cidade*/
 INSERT INTO cities(name_city, country) values ('Napoli', 'Italia');
 
-INSERT INTO bookings (start_date, start_time, end_date) VALUES ('2023-03-23', '14:30:00', '2023-03-25');
+INSERT INTO cars (name_car, descritpion, price, year_car, highlight, category_id, city_id) values ('GT 400', 'descricao qualquer', 1250.58, 2020, true, 1,1);
 
-INSERT INTO cars (name_car, descritpion, price, year_car, highlight, category_id, city_id, user_id, booking_id) values ('GT 400', 'descricao qualquer', 1250.58, 2020, true, 1,1, 1, 1);
+INSERT INTO bookings (start_date, start_time, end_date, car_id, user_id) VALUES ('2023-03-23', '14:30:00', '2023-03-25', 1, 1);
 
-INSERT INTO caracteristics(name_caracteristcs, icon) values ('potência de 340 cv e torque de 44,1 m', 'testeICon');
+INSERT INTO caracteristics(name_caracteristcs, icon) values ('potência de 340 cv e torque de 44,1 m', '');
 
 INSERT INTO car_caracteristics(car_id, caracteristics_id) values (1,1);
 
-INSERT INTO images(title, url, car_id) values ('imagem da ferrari gt 400', '  ',1);
+INSERT INTO images(title, url, car_id) values ('imagem da ferrari gt 400', '',1);
 
-select * from cars;
+
 
 
 
