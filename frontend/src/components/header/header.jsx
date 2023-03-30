9dda176ee9404b1f1080bb0147afba9259f2bc53
camila <cftinelli@outlook.com>
2023-03-29 22:16:35 -0300

    header

--- frontend/src/components/header/header.jsx
+++ frontend/src/components/header/header.jsx
@@ -5,37 +5,39 @@ import './header.css'
 import { Link, Navigate } from 'react-router-dom'
 import Image1 from '../../assets/logoWhiteLetters.png'
 //import Image2 from '../../assets/logoWhiteBox.png'
-import { getToken, isAuthenticated, getTokenName, getTokenSurname, logout} from '../../services/auth';
+import { getToken, isAuthenticated, getTokenName, getTokenSurname, logout } from '../../services/auth';
 import HeaderModal from '../headerModal/headerModal';
 import { useNavigate } from "react-router-dom";
 
 
 
 function Header() {
-  
+
   const navRef = useRef();
   const navigate = useNavigate();
-  const [visible, setVisible] = useState(false);  
+  const [visible, setVisible] = useState(false);
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   const [token, setToken] = useState(getToken());
   const [firstName, setFirstaName] = useState(getTokenName());
   const [surname, setSurname] = useState(getTokenSurname());
-  const[nameInitials, setInitials] = useState("");
+  const [nameInitials, setInitials] = useState("");
+  const [fullName, setFullName] = useState(getTokenName() + ' ' + getTokenSurname());
 
 
-   
+
+
   useEffect(() => {
-	  const handleStorage = () => {
-		setToken(getToken())
-	}
+    const handleStorage = () => {
+      setToken(getToken())
+    }
     window.addEventListener('storage', handleStorage())
     return () => window.removeEventListener('storage', handleStorage())
   }, [])
 
-  
-  useEffect(() => { 
-    setIsAuthenticated(!!token); 
-    if(isAuthenticated){
+
+  useEffect(() => {
+    setIsAuthenticated(!!token);
+    if (isAuthenticated) {
       const nameInitials = `${firstName.charAt(0)}${surname.charAt(0)}`;
       setInitials(nameInitials);
       sessionStorage.setItem('nameInitials', nameInitials);
@@ -56,7 +58,7 @@ function Header() {
     <header className="header">
       <nav className="nav " >
 
-      <a href="/"><img src={Image1} width="100" height="95" alt="LuxCars" className="brand" /></a>
+        <a href="/"><img src={Image1} width="100" height="95" alt="LuxCars" className="brand" /></a>
 
         <ul className="navList" ref={navRef}>
           <li className="navItem">
@@ -75,7 +77,7 @@ function Header() {
             <Link to="/" className="navLink">Duvidas</Link>
           </li>
 
-          {!isAuthenticated &&  <ul className="navList1 grid">
+          {!isAuthenticated && <ul className="navList1 grid">
             <li className="navItem1">
               <Link to="/login" className="navLink1">Login</Link>
             </li>
@@ -84,18 +86,18 @@ function Header() {
               <Link to="/" className="navLink1">Minhas Reservas</Link>
             </li>
           </ul>}
-
-          {isAuthenticated && 
-          <div className="user-info"><Avatar nameInitials={nameInitials} onLogout={handleLogout}/></div>}
-            
+          {isAuthenticated && (
+            <div className="user-info"><div className="gretting">Olá, {firstName} {surname}</div><Avatar nameInitials={nameInitials} onLogout={handleLogout} />
+            </div>)}
         </ul>
-          <div className="navToggler">
+
+        <div className="navToggler">
           <i class="uil uil-align-center-alt" onClick={() => setVisible(true)}></i>
         </div>
 
       </nav>
       {visible ? <HeaderModal onClose={() => setVisible(false)} /> : null}
-    </header>
+    </header >
   )
 }
 
