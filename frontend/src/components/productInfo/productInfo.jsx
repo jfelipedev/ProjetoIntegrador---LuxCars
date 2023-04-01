f7fc93e0f1047aa3e3d3ae6670a57f1016d5bcf0
ViniciusOcker <60163731+ViniciusOcker@users.noreply.github.com>
2023-03-31 22:49:43 -0300

    ajustes

--- frontend/src/components/productInfo/productInfo.jsx
+++ frontend/src/components/productInfo/productInfo.jsx
@@ -1,35 +1,49 @@
 import React, { useState, useEffect } from "react";
 import "./productInfo.css";
 import { Link } from "react-router-dom";
-import api from "../../services/api";
 
 function ProductInfo() {
-  const url = "https://carlux-grupo1.s3.us-east-2.amazonaws.com";
+  const baseUrl = "https://carlux-grupo1.s3.us-east-2.amazonaws.com";
   const [carInfo, setCarInfo] = useState({
-    nameCar:'',
-    descritpion:'',
-    category: '',
+    nameCar:null,
+    descritpion: null,
+    category: null,
+    city: null,
+    country: null,
+    year: null,
+    image: null
   });
-
-  useEffect(() => {
-    const fetchData = async () => {
+  const fetchData = async () => {
       const response = await fetch(
         `http://api.carlux.viniciusofagundes.com.br/car/1`
       );
       const jsonData = await response.json();
-      console.log(jsonData.category)
+      const response1 = await fetch(
+        `http://api.carlux.viniciusofagundes.com.br/city/${jsonData.city.id}`
+      );
+      const jsonData1 = await response1.json();
       setCarInfo({
         nameCar: jsonData.nameCar,
         descritpion: jsonData.descritpion,
         category: jsonData.category.qualification,
+        city: jsonData.city.nameCity,
+        country: jsonData1.country,
+        year: jsonData.year,
+        image: jsonData.images
       });
     };
-    fetchData();
-  }, []);
-console.log(carInfo.category.qualification)
+    useEffect(()=> {
+      fetchData();
+    }, []);
   return (
     <div className="productInfosection">
-      {/* <img src={url + carInfo.images.url} alt=""/> */}
+    {
+      (carInfo.image !== null)?
+        carInfo.image.map(({id, title, url})=>{
+          return <img key={id} src={baseUrl + url} alt={title} />
+        })
+        : ""
+    }
       <h1 className="productInfoTitle">{carInfo.nameCar}</h1>
       <div className="ProductInfoContainer container">
         <div className="productInfoDescription">
@@ -48,7 +62,7 @@ console.log(carInfo.category.qualification)
 
           <div className="pro0ductInfoYear">
             <h3 className="descript">
-              Ano <span className="textProduct"></span>{" "}
+              Ano <span className="textProduct"></span>{carInfo.year}
             </h3>
           </div>
 
@@ -59,8 +73,8 @@ console.log(carInfo.category.qualification)
           </div>
         </div>
         <div className="productInfoLocal">
-          <button className="productInfobutton">Conversivel</button>
-          <button className="productInfobutton">São Paulo, Brasil</button>
+          <button className="productInfobutton">{carInfo.category}</button>
+          <button className="productInfobutton">{carInfo.city}, {carInfo.country}</button>
           <button className="productInfobutton">Check-in Check-out</button>
           <Link to="/rent">
             <button className="productInfobutton black">
